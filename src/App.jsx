221ce1c8efb223fcaa038b112d7b86265780961f
Dung Văn <dungvan.170204@gmail.com>
2025-07-16 22:13:54 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+import "./App.css";
+import { Search } from "./components/Search";
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <main>
+      <div className="pattern"></div>
+      <div className="wrapper">
+        <header>
+          <img src="/hero-img.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You’ll Love
+            Without the Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
+  );
+}
+
+export default App;
